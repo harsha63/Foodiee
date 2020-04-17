8eb56d4180e206d543f50e2af5255e1fc7de6e41
saumya gangrade <saumyagangrade8@gmail.com>
2020-04-18 02:15:06 +0530

    RestaurantList added

--- app/src/main/java/com/example/login/RestaurantAdapter.java
+++ app/src/main/java/com/example/login/RestaurantAdapter.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder> {
     private ArrayList<Restaurant> mRestList;
     FirebaseFirestore fStore;
-    FirebaseAuth mFAuth;
+
 
 
     public static class RestaurantViewHolder extends RecyclerView.ViewHolder {
